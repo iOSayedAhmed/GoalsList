5a785620881f9ce5dfc4d6b8e363261d34de3040
iOSayedAhmed <elsayed1ahmed0@gmail.com>
2021-11-06 10:16:02 +0200

    Initial Commit

---
+++ GoalsList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoalsList
+//
+//  Created by Develop on 11/6/21.
+//  Copyright © 2021 Develop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
